b39cee847cc0f8ca7f2b0d21c6ed11108c616186
Juan Gutierrez <juan.gutierrez@nxp.com>
2018-01-10 22:33:56 +0000

    avs-tools: Update json config from by setCredentials script
    Use the AlexaClientSDKConfig.json from source directly and update based
    on its variables. Previously a pre-filled file (on build time) was used
    but this was prone to fail if the json file is changed on a future release.
    To minimize this possible failure, the AlexaClientSDKConfig.json is now
    filled on runtime by the setCredentials script.
    
    Signed-off-by: Juan Gutierrez <juan.gutierrez@nxp.com>

--- recipes-avs/avs-tools/files/setCredentials.sh
+++ recipes-avs/avs-tools/files/setCredentials.sh
@@ -7,44 +7,62 @@ echo "===================================================================="
 echo ""
 
 AVS_SDK_DIR="/home/root/Alexa_SDK/avs-sdk-client"
+AVS_SOURCE_DIR="/home/root/Alexa_SDK/avs-device-sdk"
 SDK_CONFIG_FILE="Integration/AlexaClientSDKConfig.json"
 AVS_CONF="/etc/alexa_sdk/avs.conf"
 
-AVS_PRODUCT_ID="my_device"
-AVS_CLIENT_ID="amzn1.application-oa2-client.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-AVS_CLIENT_SECRET="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+SDK_CONFIG_PRODUCT_ID="my_device"
+SDK_CONFIG_CLIENT_ID="amzn1.application-oa2-client.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+SDK_CONFIG_CLIENT_SECRET="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+SDK_CONFIG_DEVICE_SERIAL_NUMBER="123456"
+SDK_SQLITE_DATABASE_FILE_PATH="/database/myAlertsDatabase"
+SDK_SQLITE_SETTINGS_DATABASE_FILE_PATH="/database/mySettingsDatabase"
+SETTING_LOCALE_VALUE="en-US"
+SDK_CERTIFIED_SENDER_DATABASE_FILE_PATH="/database/myCertDatabase"
 
 while true; do
 
 if [ -r "$AVS_CONF" ]; then
         array=($(<$AVS_CONF))
 
-        AVS_PRODUCT_ID=${array[0]}
-        AVS_CLIENT_ID=${array[1]}
-        AVS_CLIENT_SECRET=${array[2]}
+        SDK_CONFIG_PRODUCT_ID=${array[0]}
+        SDK_CONFIG_CLIENT_ID=${array[1]}
+        SDK_CONFIG_CLIENT_SECRET=${array[2]}
+        SETTING_LOCALE_VALUE=${array[3]}
 fi
 
-  cp /etc/alexa_sdk/AlexaClientSDKConfig.json.Original $AVS_SDK_DIR/$SDK_CONFIG_FILE
+  cp $AVS_SOURCE_DIR/$SDK_CONFIG_FILE $AVS_SDK_DIR/$SDK_CONFIG_FILE
+
+  sed -e "s#\${SDK_CONFIG_DEVICE_SERIAL_NUMBER}#${SDK_CONFIG_DEVICE_SERIAL_NUMBER}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
+  sed -e "s#\${SDK_SQLITE_DATABASE_FILE_PATH}#${SDK_SQLITE_DATABASE_FILE_PATH}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
+  sed -e "s#\${SDK_SQLITE_SETTINGS_DATABASE_FILE_PATH}#${SDK_SQLITE_SETTINGS_DATABASE_FILE_PATH}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
+#  sed -e "s#\${SETTING_LOCALE_VALUE}#${SETTING_LOCALE_VALUE}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
+  sed -e "s#\${SDK_CERTIFIED_SENDER_DATABASE_FILE_PATH}#${SDK_CERTIFIED_SENDER_DATABASE_FILE_PATH}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
 
   while read -r -t 0; do read -r; done
 
-  read -p "Enter your Product ID[$AVS_PRODUCT_ID]: " usrInput
-  echo "${usrInput:-$AVS_PRODUCT_ID}" > $AVS_CONF
-  sed -e "s/SDK_CONFIG_DEVICE_TYPE_ID/${usrInput:-$AVS_PRODUCT_ID}/g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
+  read -p "Enter your Product ID[$SDK_CONFIG_PRODUCT_ID]: " usrInput
+  echo "${usrInput:-$SDK_CONFIG_PRODUCT_ID}" > $AVS_CONF
+  sed -e "s#\${SDK_CONFIG_PRODUCT_ID}#${usrInput:-$SDK_CONFIG_PRODUCT_ID}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
 
-  read -p "Enter your Clent Id[$AVS_CLIENT_ID]: " usrInput
-  echo "${usrInput:-$AVS_CLIENT_ID}" >> $AVS_CONF
-  sed -e "s/SDK_CONFIG_CLIENT_ID/${usrInput:-$AVS_CLIENT_ID}/g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
+  read -p "Enter your Clent Id[$SDK_CONFIG_CLIENT_ID]: " usrInput
+  echo "${usrInput:-$SDK_CONFIG_CLIENT_ID}" >> $AVS_CONF
+  sed -e "s#\${SDK_CONFIG_CLIENT_ID}#${usrInput:-$SDK_CONFIG_CLIENT_ID}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
 
-  read -p "Enter your Client Secret[$AVS_CLIENT_SECRET]: " usrInput
-  echo "${usrInput:-$AVS_CLIENT_SECRET}" >> $AVS_CONF
-  sed -e "s/SDK_CONFIG_CLIENT_SECRET/${usrInput:-$AVS_CLIENT_SECRET}/g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
+  read -p "Enter your Client Secret[$SDK_CONFIG_CLIENT_SECRET]: " usrInput
+  echo "${usrInput:-$SDK_CONFIG_CLIENT_SECRET}" >> $AVS_CONF
+  sed -e "s#\${SDK_CONFIG_CLIENT_SECRET}#${usrInput:-$SDK_CONFIG_CLIENT_SECRET}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
 
-   read -p "Are your Credential OK [Y/N]? " usrInput
-    case $usrInput in
-        [Yy]* )  break;;
-        [Nn]* )  ;;
-        * ) echo "Please answer yes or no.";;
-    esac
+  read -p "Enter your preferred locale (en-US, en-GB, de-DE, en-IN en-CA)[$SETTING_LOCALE_VALUE]: " usrInput
+  echo "${usrInput:-$SETTING_LOCALE_VALUE}" >> $AVS_CONF
+  sed -e "s#\${SETTING_LOCALE_VALUE}#${usrInput:-$SETTING_LOCALE_VALUE}#g" -i $AVS_SDK_DIR/$SDK_CONFIG_FILE
+
+  read -p "Are your Settings OK [Y/N]? " usrInput
+  case $usrInput in
+      [Yy]* )  break;;
+      [Nn]* )  ;;
+      * ) echo "Please answer yes or no.";;
+  esac
+
 done
 
